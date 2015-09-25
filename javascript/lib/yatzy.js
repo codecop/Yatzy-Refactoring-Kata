d1330d4a525f1b2b173e86448ccc14d97eeae9f9
Peter Kofler <peter.kofler@code-cop.org>
2015-09-25 13:54:07 +0200

    Remove primitive field of dices.

--- javascript/lib/yatzy.js
+++ javascript/lib/yatzy.js
@@ -64,14 +64,12 @@ var Dices = function(eyesOfDice1, eyesOfDice2, eyesOfDice3, eyesOfDice4, eyesOfD
 		}, 0);
 	}
 
-}
-
+};
 
 var Yatzy = function(eyesOfDice1, eyesOfDice2, eyesOfDice3, eyesOfDice4, eyesOfDice5) {
 	"use strict";
 
-    var dice = [eyesOfDice1, eyesOfDice2, eyesOfDice3, eyesOfDice4, eyesOfDice5];
-    var xxx = new Dices(eyesOfDice1, eyesOfDice2, eyesOfDice3, eyesOfDice4, eyesOfDice5);
+    var dice = new Dices(eyesOfDice1, eyesOfDice2, eyesOfDice3, eyesOfDice4, eyesOfDice5);
     // with internal max, sum, first, this separates rules from array logic
 
 	// TODO sort functions by public/private, private down - after dices has been pulled out
@@ -84,40 +82,40 @@ var Yatzy = function(eyesOfDice1, eyesOfDice2, eyesOfDice3, eyesOfDice4, eyesOfD
 
     this.ones = function()
     {
-		return sum(xxx.diceWith(1));
+		return sum(dice.diceWith(1));
     };
 
     this.twos = function()
     {
-		return sum(xxx.diceWith(2));
+		return sum(dice.diceWith(2));
     };
 
     this.threes = function()
     {
-		return sum(xxx.diceWith(3));
+		return sum(dice.diceWith(3));
     };
 
     this.fours = function()
     {
-		return sum(xxx.diceWith(4));
+		return sum(dice.diceWith(4));
     };
 
     this.fives = function()
     {
-		return sum(xxx.diceWith(5));
+		return sum(dice.diceWith(5));
     };
 
     this.sixes = function()
     {
-		return sum(xxx.diceWith(6));
+		return sum(dice.diceWith(6));
     };
 
     this.chance = function() {
-		return xxx.sum();
+		return dice.sum();
 	};
 
     this.yatzy = function() {
-		if (xxx.allDiceSameLike(xxx.firstDice())) {
+		if (dice.allDiceSameLike(dice.firstDice())) {
 			return 50;
 		}
 		return 0;
@@ -128,19 +126,19 @@ var Yatzy = function(eyesOfDice1, eyesOfDice2, eyesOfDice3, eyesOfDice4, eyesOfD
     }
 
 	this.onePair = function() {
-		return max(xxx.eyesWithCountOfAtLeast(2)) * 2;
+		return max(dice.eyesWithCountOfAtLeast(2)) * 2;
 	};
 
 	this.fourOfAKind = function() {
-		return max(xxx.eyesWithCountOfAtLeast(4)) * 4;
+		return max(dice.eyesWithCountOfAtLeast(4)) * 4;
 	};
 
 	this.threeOfAKind = function() {
-		return max(xxx.eyesWithCountOfAtLeast(3)) * 3;
+		return max(dice.eyesWithCountOfAtLeast(3)) * 3;
 	};
 
 	this.twoPair = function() {
-	    var eyesOfExistingPairs = xxx.eyesWithCountOfAtLeast(2);
+	    var eyesOfExistingPairs = dice.eyesWithCountOfAtLeast(2);
 	    if (eyesOfExistingPairs.length === 2) {
 	         return sum(eyesOfExistingPairs) * 2;
 	    }
@@ -148,26 +146,26 @@ var Yatzy = function(eyesOfDice1, eyesOfDice2, eyesOfDice3, eyesOfDice4, eyesOfD
      };
 
     this.smallStraight = function() {
-        var eyes = xxx.eyesWithCountOfExactly(1);
+        var eyes = dice.eyesWithCountOfExactly(1);
         if ([1,2,3,4,5].equals(eyes)) {
-            return xxx.sum();
+            return dice.sum();
         }
         return 0;
     };
 
     this.largeStraight = function() {
-        var eyes = xxx.eyesWithCountOfExactly(1);
+        var eyes = dice.eyesWithCountOfExactly(1);
         if ([2,3,4,5,6].equals(eyes)) {
-            return xxx.sum();
+            return dice.sum();
         }
         return 0;
     };
 
     this.fullHouse = function() {
-        var pairs = xxx.eyesWithCountOfExactly(2);
-        var threes = xxx.eyesWithCountOfExactly(3);
+        var pairs = dice.eyesWithCountOfExactly(2);
+        var threes = dice.eyesWithCountOfExactly(3);
         if (pairs.length === 1 && threes.length === 1) {
-            return xxx.sum();
+            return dice.sum();
         }
         return 0;
     };
